833ee870181265823938cf59495b37b45b1abb09
Adrian_Mendez <admendez89@gmail.com>
2021-08-31 14:51:21 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+//ejemplo
+const Titulo = () => {
+  return <h1>Hola</h1>
+}
+
 function App() {
    
   return (
     <div className="App">
       <header className="App-header">
+        <Titulo/>
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
